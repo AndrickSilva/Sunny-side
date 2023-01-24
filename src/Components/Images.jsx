ea7d2c20b4e6a5c35030429c9f52111cc589dfd1
AndrickSilva <silva.andrick2001@gmail.com>
2023-01-24 12:33:28 +0530

    img section complete

--- src/Components/Images.jsx
+++ src/Components/Images.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Images = () => {
-  return (
-    <div>Images</div>
-  )
+    return (
+        <div className='flex justify-center items-center w-full'>
+            <img src="/public/desktop/image-gallery-milkbottles.jpg" alt="img" className='w-full h-96' />
+            <img src="/public/desktop/image-gallery-orange.jpg" alt="img" className='w-full h-96' />
+            <img src="/public/desktop/image-gallery-cone.jpg" alt="img" className='w-full h-96' />
+            <img src="/public/desktop/image-gallery-sugarcubes.jpg" alt="img" className='w-full h-96' />
+        </div>
+    )
 }
 
 export default Images
